077436b100091f912ffd04726bc8f7e06331e775
Demid Gerasimenko <gerasimenko.demid2013@yandex.ru>
2019-10-22 18:39:19 +0300

    hcru

---
+++ js.js
@@ -0,0 +1,96 @@
+function token() {
+    var n1e = document.querySelector('input[name="n1"]').value;
+    var rez;
+    rez = +n1e + 129;
+    var res_e = document.querySelector('input[name="n1"]');
+    res_e.value = rez;
+  }
+  
+  function token2() {
+    var n1e = document.querySelector('input[name="n1"]').value;
+    var rez;
+    rez = +n1e + 210;
+    var res_e = document.querySelector('input[name="n1"]');
+    res_e.value = rez;
+  }
+  
+  function token3() {
+    var n1e = document.querySelector('input[name="n1"]').value;
+    var rez;
+    rez = +n1e + 324;
+    var res_e = document.querySelector('input[name="n1"]');
+    res_e.value = rez;
+  }
+  
+  function token4() {
+    var n1e = document.querySelector('input[name="n1"]').value;
+    var rez;
+    rez = +n1e + 749;
+    var res_e = document.querySelector('input[name="n1"]');
+    res_e.value = rez;
+  }
+  
+  function Month() {
+    var n1e = document.querySelector('input[name="n1"]').value;
+    var rez;
+    rez = +n1e + 215;
+    var res_e = document.querySelector('input[name="n1"]');
+    res_e.value = rez;
+  }
+  
+  function Month6() {
+    var n1e = document.querySelector('input[name="n1"]').value;
+    var rez;
+    rez = +n1e + 1266;
+    var res_e = document.querySelector('input[name="n1"]');
+    res_e.value = rez;
+  }
+  
+  function Month12() {
+    var n1e = document.querySelector('input[name="n1"]').value;
+    var rez;
+    rez = +n1e + 2064;
+    var res_e = document.querySelector('input[name="n1"]');
+    res_e.value = rez;
+  }
+  
+  function Month32() {
+    var n1e = document.querySelector('input[name="n1"]').value;
+    var rez;
+    rez = +n1e + 4644;
+    var res_e = document.querySelector('input[name="n1"]');
+    res_e.value = rez;
+  }
+  
+  function Add1() {
+    var n1e = document.querySelector('input[name="n1"]').value;
+    var rez;
+    rez = +n1e + 0;
+    var res_e = document.querySelector('input[name="n1"]');
+    res_e.value = rez;
+  }
+  
+  function Add2() {
+    var n1e = document.querySelector('input[name="n1"]').value;
+    var rez;
+    rez = +n1e + 79;
+    var res_e = document.querySelector('input[name="n1"]');
+    res_e.value = rez;
+  }
+  
+  function Add3() {
+    var n1e = document.querySelector('input[name="n1"]').value;
+    var rez;
+    rez = +n1e + 259;
+    var res_e = document.querySelector('input[name="n1"]');
+    res_e.value = rez;
+  }
+  
+  function Add4(){
+    var n1e = document.querySelector('input[name="n1"]').value;
+    var rez;
+    rez = +n1e + 0;
+    var res_e = document.querySelector('input[name="n1"]');
+    res_e.value = rez;
+  }
+  
